f6338f38d22aac55e9412007d43651e0637a0447
GamuIsAlreadyTaken <victordiez.ceep@gmail.com>
2021-11-07 21:51:10 +0100

    Base iniciada

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TruthCheck-RolAssistant
  Projecto de fin de grado
+
+ Se trata de una aplicación que reune varias herramientas para facilitar los juegos de rol
+ 
+
+ Y donde **todos** los recursos, como personajes, items, dados, etc. pueden ser compartidos 
+
+ La aplicación será una *Progresive web app* para que esta sea accesible desde cualquier dispositivo
---
+++ app.js
@@ -0,0 +1,26 @@
+//Node modules for the app to run
+const http = require('http');
+const fs = require('fs');
+const mime = require('mime');
+
+
+const host = '127.0.0.1';
+const port = 3000;
+const basepath = __dirname.replace(/\\/g,'/',);
+
+
+
+const server = http.createServer((req,res) =>{
+    if(req.url === '/') req.url = '/index.html';
+
+    fs.readFile(basepath + req.url, (err, data) => {
+        res.statusCode = 200;
+        
+        res.setHeader('Content-type',mime.getType(req.url));
+        res.end(data);
+    });
+});
+
+server.listen(port, host, () =>{
+    console.log(`server runing on ${host}:${port}`);
+});
