2409fd24d5e43749cb8cc25241222c238cc3c555
GamuCEEP <victordiez.ceep@gmail.com>
2021-11-15 14:21:41 +0100

    Pruebas

--- DataBase/db.js
+++ DataBase/db.js
@@ -1,2 +1,27 @@
 
+const fs = require("fs");
+const path = require("path");
+
+
+function createDirectory(name){
+    fs.mkdir(name,{recursive: true},(err) =>{
+        throw "Directory could not be created";
+    });
+}
+function createFile(){
+
+}
+function deleteFile(){
+
+}
+function writeLine(){
+
+}
+function deleteLine(){
+
+}
+function modifyLine(){
+
+}
+
 
--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 //Node modules for the app to run
 const http = require('http');
 const fs = require('fs');
-const mime = require('mime');
 
 
 const host = '127.0.0.1';
 const port = 3000;
-const basepath = __dirname.replace(/\\/g,'/',);
+const basepath = __dirname.replace(/\\/g, '/');//Change all \ with /
 
+const extensionRegex = /\.(\w+)$/;//Gets the extension (a '.'followed by letters at the end of string)
 
+const contentTypes = {
+    "html": "text/html",
+    "css": "text/css",
+    "js": "text/javascript",
+    "": "text/plain"
+};
 
-const server = http.createServer((req,res) =>{
-    if(req.url === '/') req.url = '/index.html';
-    
+const server = http.createServer((req, res) => {
+    if (req.url === '/') req.url = '/index.html';
+
     fs.readFile(basepath + req.url, (err, data) => {
         res.statusCode = 200;
-        
-        res.setHeader('Content-type',mime.getType(req.url));
+        let a = contentTypes[req.url.match(extensionRegex) || ""];
+        console.log(a);
+        res.setHeader('Content-type', a) ;
         res.end(data);
     });
 });
 
-server.listen(port, host, () =>{
+server.listen(port, host, () => {
     console.log(`server runing on ${host}:${port}`);
 });
