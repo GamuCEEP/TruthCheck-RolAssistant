14a979f37924dd31be2bbadc2a31b2ecbd095044
GamuCEEP <91058173+GamuCEEP@users.noreply.github.com>
2022-06-18 19:40:46 +0200

    a

--- lock_update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Updating lock file.."
-deno cache --lock=lock.json --lock-write --unstable ./deps.ts
-
-exit 0
--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Running tests in docker container..."
-docker-compose -f docker-compose.yml -f docker-compose.test.yml up --build --exit-code-from deno-rest --abort-on-container-exit
-
-exit 0
